d791e9d51cd7063ada7cc7635050d697bcfc47c2
ryanburns31892 <rburns@chapman.edu>
2016-11-30 17:53:56 -0800

    Adding maps and location and stuff

---
+++ NotesApplication/NotesApplication/NoteMapViewController.swift
@@ -0,0 +1,18 @@
+//
+//  NoteMapViewController.swift
+//  NotesApplication
+//
+//  Created by Ryan Burns on 11/30/16.
+//  Copyright © 2016 Chapman University. All rights reserved.
+//
+
+import UIKit
+
+class NoteMapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
